9d03fc1258e5e1ba9ed2235b26d3e2e4cbc4143e
Prankul Singh <prankulsinghps@gmail.com>
2016-08-24 22:07:26 +0530

    Initial push after xmls

---
+++ app/src/main/java/in/ac/iiitd/prankul/primetime/HintActivity.java
@@ -0,0 +1,13 @@
+package in.ac.iiitd.prankul.primetime;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HintActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hint);
+    }
+}
